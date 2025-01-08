4e5612de024d96de780df71659f546e0bedbc340
unknown <bankkok159z@gmail.com>
2025-01-08 12:13:55 +0700

    version2 : Create Pj

---
+++ lib/home_Ui.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class bbbbbbb extends StatefulWidget {
+  const bbbbbbb({super.key});
+
+  @override
+  State<bbbbbbb> createState() => _bbbbbbbState();
+}
+
+class _bbbbbbbState extends State<bbbbbbb> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
